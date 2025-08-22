17c651bf63fda3c076e8dbfc192351fabc66fc5a
ledinhloc <ledinhloc7@gmail.com>
2025-08-22 17:37:59 +0700

    update config

---
+++ services/iam-service/src/main/java/com/eefood/iamservice/config/OpenAPIConfig.java
@@ -0,0 +1,18 @@
+package com.eefood.iamservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+//  @Bean
+//  public OpenAPI setupAPI() {
+//    return new OpenAPI()
+//      .info(
+//        new Info()
+//          .title("Iam Service API")
+//          .description("This is the REST API for Iam Service")
+//          .version("v0.0.1"));
+//  }
+}
