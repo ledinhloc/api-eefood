e9afafed58a6c3e9d1fdf257fd40b618444fc512
ledinhloc <ledinhloc7@gmail.com>
2025-08-22 15:56:33 +0700

    add repo config, iam, noti, reaction, recipe

---
+++ services/notification-service/src/main/groovy/com/eefood/notificationservice/NotificationServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.eefood.notificationservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NotificationServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication, args)
+	}
+
+}
