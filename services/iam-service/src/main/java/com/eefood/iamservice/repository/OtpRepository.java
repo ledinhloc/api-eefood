05667e8125d12eda96740395997eb98bba2f7875
AKhoa2109 <anhkhoa2109.it@gmail.com>
2025-08-31 09:40:50 +0700

    Update feature: Add rate limit otp send

--- services/iam-service/src/main/java/com/eefood/iamservice/repository/OtpRepository.java
+++ services/iam-service/src/main/java/com/eefood/iamservice/repository/OtpRepository.java
@@ -5,9 +5,12 @@ import com.eefood.iamservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface OtpRepository extends JpaRepository<Otp, Long> {
     Optional<Otp> findByUserAndOtpNumAndIsDeletedFalse(User user, String otpCode);
+    int countByUserAndCreatedAtAfter(User user, LocalDateTime after); // dùng cho rate-limit
+    Optional<Otp> findTopByUserOrderByCreatedAtDesc(User user); // lấy OTP mới nhất
 }
