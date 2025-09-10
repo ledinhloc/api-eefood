e6d82a09cbc8c8d60d1053ee9b4d8c167c5d8104
AKhoa2109 <anhkhoa2109.it@gmail.com>
2025-09-10 22:17:51 +0700

    Commit to switch branch

---
+++ services/recipe-service/src/main/java/com/eefood/recipeservice/enums/Difficulty.java
@@ -0,0 +1,5 @@
+package com.eefood.recipeservice.enums;
+
+public enum Difficulty {
+    EASY,MEDIUM,HARD
+}
---
+++ services/recipe-service/src/main/java/com/eefood/recipeservice/model/Ingredient.java
@@ -0,0 +1,28 @@
+package com.eefood.recipeservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "ingredients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Ingredient extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    private String description;
+
+    private String image;
+}
