4856fcfc4e5ff72d0add1e7a14b4b962058a9fa3
AKhoa2109 <anhkhoa2109.it@gmail.com>
2025-10-12 09:56:34 +0700

    update(): edit NotificationsType

--- services/notification-service/src/main/resources/db/migration/V20251001_0905__insert_data.sql
+++ services/notification-service/src/main/resources/db/migration/V20251001_0905__insert_data.sql
@@ -6,8 +6,8 @@ VALUES
     (now(), now(), 'System', 'System', false, 'Thông báo hệ thống', 'Hệ thống vừa cập nhật tính năng mới', '/System/update', null, null, 'SYSTEM'),
     (now(), now(), 'System', 'System', false, 'Lưu công thức', 'Bạn đã lưu công thức "Phở bò" vào danh sách mua sắm', '/recipes/5', null, 'https://img.com/post5.png', 'SAVE_RECIPE'),
     (now(), now(), 'System', 'System', false, 'Chia sẻ công thức', 'Người dùng D đã chia sẻ công thức của bạn', '/recipes/6', 'https://img.com/u4.png', 'https://img.com/post6.png', 'SHARE_RECIPE'),
-    (now(), now(), 'System', 'System', false, 'Chào mừng', 'Chào mừng bạn quay lại EEFOOD, chúc bạn một ngày tốt lành!', '/welcome', null, null, 'SYSTEM'),
-    (now(), now(), 'System', 'System', false, 'Công thức trong ngày', 'Gợi ý hôm nay: Bánh mì kẹp thịt gà thơm ngon', '/recipes/7', null, 'https://img.com/post7.png', 'SYSTEM'),
+    (now(), now(), 'System', 'System', false, 'Chào mừng', 'Chào mừng bạn quay lại EEFOOD, chúc bạn một ngày tốt lành!', '/welcome', null, null, 'WELCOME'),
+    (now(), now(), 'System', 'System', false, 'Công thức trong ngày', 'Gợi ý hôm nay: Bánh mì kẹp thịt gà thơm ngon', '/recipes/7', null, 'https://img.com/post7.png', 'RECOMMEND_RECIPE'),
     (now(), now(), 'System', 'System', false, 'Phản ứng mới', 'Người dùng E đã thả tim vào công thức của bạn', '/posts/8', 'https://img.com/u5.png', 'https://img.com/post8.png', 'REACTION'),
     (now(), now(), 'System', 'System', false, 'Bình luận mới', 'Người dùng F đã bình luận: "Công thức này rất ngon!"', '/posts/9', 'https://img.com/u6.png', 'https://img.com/post9.png', 'COMMENT');
 
