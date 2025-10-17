70f772fc2956f1b6979537b6f2a6213fbbf34487
AKhoa2109 <anhkhoa2109.it@gmail.com>
2025-10-17 09:20:01 +0700

    feat(): get comment and add comment

---
+++ services/reaction-service/src/main/java/com/eefood/reactionservice/controller/CommentController.java
@@ -0,0 +1,52 @@
+package com.eefood.reactionservice.controller;
+
+import com.eefood.reactionservice.dto.request.CommentRequest;
+import com.eefood.reactionservice.dto.response.CommentResponse;
+import com.eefood.reactionservice.dto.response.ResponseData;
+import com.eefood.reactionservice.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping("")
+    public ResponseData<CommentResponse> createComment(@RequestBody CommentRequest commentRequest) {
+        var result = commentService.addComment(commentRequest);
+        return new ResponseData<>(HttpStatus.OK.value(), "Success", result);
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseData<Page<CommentResponse>> getRootComments(
+    @PathVariable Long postId,
+    @RequestParam(defaultValue = "1") int page,
+    @RequestParam(defaultValue = "10") int limit,
+    @RequestParam(defaultValue = "createdAt") String sortBy,
+    @RequestParam(defaultValue = "DESC") Sort.Direction sortDirection
+    ) {
+        Pageable pageable = PageRequest.of(page-1, limit, Sort.by(sortDirection, sortBy));
+        Page<CommentResponse> result = commentService.getPostComments(postId, pageable);
+        return new ResponseData<>(HttpStatus.OK.value(), "Success", result);
+    }
+
+    @GetMapping("/{commentId}/replies")
+    public ResponseData<Page<CommentResponse>> getRepliesComments(
+            @PathVariable Long commentId,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int limit,
+            @RequestParam(defaultValue = "createdAt") String sortBy,
+            @RequestParam(defaultValue = "DESC") Sort.Direction sortDirection
+    ) {
+        Pageable pageable = PageRequest.of(page-1, limit, Sort.by(sortDirection, sortBy));
+        Page<CommentResponse> result = commentService.getCommentReplies(commentId, pageable);
+        return new ResponseData<>(HttpStatus.OK.value(), "Success", result);
+    }
+}
