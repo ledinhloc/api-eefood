106653559e1aac99a5941af03adaf088ebb6560b
AKhoa2109 <anhkhoa2109.it@gmail.com>
2025-10-26 21:03:19 +0700

    commit to switch branch

---
+++ services/reaction-service/src/main/java/com/eefood/reactionservice/controller/ShareController.java
@@ -0,0 +1,28 @@
+package com.eefood.reactionservice.controller;
+
+import com.eefood.reactionservice.dto.request.ShareRequest;
+import com.eefood.reactionservice.dto.response.ResponseData;
+import com.eefood.reactionservice.dto.response.ShareResponse;
+import com.eefood.reactionservice.service.ShareService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/shares")
+@RequiredArgsConstructor
+public class ShareController {
+    private final ShareService shareService;
+
+    @PostMapping
+    public ResponseData<ShareResponse> sharePost(@RequestBody ShareRequest shareRequest) {
+        ShareResponse shareResponse = shareService.sharePost(shareRequest);
+        return new ResponseData<>(HttpStatus.OK.value(), "Share successfully", shareResponse);
+    }
+
+    @GetMapping("/count/{postId}")
+    public ResponseData<Long> countPost(@PathVariable Long postId) {
+        Long count  = shareService.getShareCountByPostId(postId);
+        return new ResponseData<>(HttpStatus.OK.value(), "Success", count);
+    }
+}
