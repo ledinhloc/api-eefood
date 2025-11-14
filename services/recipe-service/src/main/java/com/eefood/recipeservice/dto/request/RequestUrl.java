e168a836604dab83d27d5aa714180c8f9e743d24
ledinhloc <ledinhloc7@gmail.com>
2025-11-14 18:03:59 +0700

    feat():extract url

---
+++ services/recipe-service/src/main/java/com/eefood/recipeservice/dto/request/RequestUrl.java
@@ -0,0 +1,14 @@
+package com.eefood.recipeservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RequestUrl {
+  private String url;
+}
---
+++ services/recipe-service/src/main/java/com/eefood/recipeservice/dto/response/IngredientExtractDTO.java
@@ -0,0 +1,16 @@
+package com.eefood.recipeservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class IngredientExtractDTO {
+  private String name;
+  private Integer quantity;
+  private String unit;
+}
