c445a4cba22be8cfac427cd1c6b8762053dc52ee
AKhoa2109 <anhkhoa2109.it@gmail.com>
2025-11-15 08:41:14 +0700

    commit to pull code

--- services/reaction-service/src/main/java/com/eefood/reactionservice/service/PostService.java
+++ services/reaction-service/src/main/java/com/eefood/reactionservice/service/PostService.java
@@ -38,6 +38,7 @@ public class PostService {
   private final SecurityUtil securityUtil;
   private final GeminiService geminiService;
   private final FollowService followService;
+  private final PostAdminSearchService postAdminSearchService;
 
   public List<PostPublishResponse> getPostsPublishByUser() {
     Long userId = securityUtil.getCurrentUserId();
@@ -214,6 +215,67 @@ public class PostService {
     return new PageImpl<>(postResponses, PageRequest.of(page - 1, size), postIds.size());
   }
 
+  public Page<PostResponse> getAllPostsByAdmin(
+          String keyword,
+          Long userId,
+          String region,
+          String difficulty,
+          String category,
+          Integer minPrepTime,
+          Integer maxPrepTime,
+          Integer minCookTime,
+          Integer maxCookTime,
+          Integer minReactionCount,
+          Integer minTotalShares,
+          String sortBy,
+          Pageable pageable
+  ) {
+    // Lấy danh sách postId từ Elasticsearch, đã paginate
+    Page<Long> esPage = postAdminSearchService.searchPostIds(
+            keyword,
+            region,
+            difficulty,
+            category,
+            minPrepTime,
+            maxPrepTime,
+            minCookTime,
+            maxCookTime,
+            minReactionCount,
+            minTotalShares,
+            sortBy,
+            pageable
+    );
+
+    List<Long> postIds = esPage.getContent();
+    long total = esPage.getTotalElements();
+    log.info("--------------PostIds : " + total);
+
+
+    if (postIds.isEmpty()) {
+      return new PageImpl<>(List.of(), pageable, 0);
+    }
+
+    // Lấy Post từ DB theo postIds
+    Specification<Post> spec = PostSpecification.isNotDeleted()
+            .and(PostSpecification.hasUserId(userId))
+            .and(PostSpecification.hasPostIds(postIds));
+
+    List<Post> posts = postRepo.findAll(spec);
+
+    // Sắp xếp theo thứ tự ES trả về
+    Map<Long, Post> postMap = posts.stream()
+            .collect(Collectors.toMap(Post::getId, p -> p));
+
+    List<Post> orderedPosts = postIds.stream()
+            .map(postMap::get)
+            .filter(Objects::nonNull)
+            .toList();
+
+    // Chuyển sang response
+    List<PostResponse> responses = mapToPostResponse(orderedPosts);
+
+    return new PageImpl<>(responses, pageable, total);
+  }
 
   private List<PostResponse> mapToPostResponse(List<Post> posts) {
     // Lấy thông tin user
