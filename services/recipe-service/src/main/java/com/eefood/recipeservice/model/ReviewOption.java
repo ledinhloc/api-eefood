f6b64d11953500f8fa219eb556f74ff1b573ee1a
AKhoa2109 <anhkhoa2109.it@gmail.com>
2026-04-20 11:03:53 +0700

    feat: add flyway db and model entity for feature review recipe by question

---
+++ services/recipe-service/src/main/java/com/eefood/recipeservice/model/ReviewOption.java
@@ -0,0 +1,30 @@
+package com.eefood.recipeservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "review_options")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class ReviewOption extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "question_id", nullable = false)
+    private ReviewQuestion question;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer starValue;
+
+    private Integer displayOrder;
+}
