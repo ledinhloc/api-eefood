ce973d951d0ff0593da0d38356f7e89f4d4d0d71
AKhoa2109 <anhkhoa2109.it@gmail.com>
2026-04-30 20:47:06 +0700

    fix: change name and logic for pagination

--- services/recipe-service/src/main/java/com/eefood/recipeservice/controller/RecipeReviewController.java
+++ services/recipe-service/src/main/java/com/eefood/recipeservice/controller/RecipeReviewController.java
@@ -31,10 +31,10 @@ public class RecipeReviewController {
     @GetMapping("/{recipeId}/list")
     public ResponseData<Page<ReviewDetailResponse>> getRecipeReviews(
             @PathVariable Long recipeId,
-            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size)
     {
-        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+        Pageable pageable = PageRequest.of(page-1, size, Sort.by("createdAt").descending());
         return new ResponseData<>(200, "Get success", recipeReviewService.getRecipeReviews(recipeId, pageable));
     }
 
