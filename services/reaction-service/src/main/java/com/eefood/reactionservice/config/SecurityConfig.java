6cbea199ee8a9184cafb582383ea26c4a8227cef
AKhoa2109 <anhkhoa2109.it@gmail.com>
2026-06-09 16:24:57 +0700

    fix: add endpoint vnpay ipn

--- services/reaction-service/src/main/java/com/eefood/reactionservice/config/SecurityConfig.java
+++ services/reaction-service/src/main/java/com/eefood/reactionservice/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
               .requestMatchers(HttpMethod.GET, "/api/v1/posts/my").permitAll()
               .requestMatchers(HttpMethod.GET, "/api/v1/livestreams/check").permitAll()
               .requestMatchers(HttpMethod.GET, "/api/v1/livestreams/active").permitAll()
+              .requestMatchers(HttpMethod.GET, "/api/v1/payment/vnpay/ipn").permitAll()
               .requestMatchers(HttpMethod.POST, "/api/v1/livestreams/subtitles/transcripts").permitAll()
               .anyRequest().authenticated());
 
